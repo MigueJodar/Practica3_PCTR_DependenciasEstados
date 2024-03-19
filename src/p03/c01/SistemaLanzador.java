c0deb1033b353d8debe1dc366bfdca75bd330401
MigueJodar <152318937+MigueJodar@users.noreply.github.com>
2024-03-19 15:08:53 +0100

    Update SistemaLanzador.java

--- src/p03/c01/SistemaLanzador.java
+++ src/p03/c01/SistemaLanzador.java
@@ -1,26 +1,26 @@
 package src.p03.c01;
 
 public class SistemaLanzador {
-	public static void main(String[] args) {
-		
-		IParque parque = new Parque(); // TODO
-		char letra_puerta = 'A';
-		
-		System.out.println("¡Parque abierto!");
-		
-		for (int i = 0; i < Integer.parseInt(args[0]); i++) {
-			
-			String puerta = ""+((char) (letra_puerta++));
-			
-			// Creación de hilos de entrada
-			ActividadEntradaPuerta entradas = new ActividadEntradaPuerta(puerta, parque);
-			new Thread (entradas).start();
-			
-			// 
-			// TODO
-			//
-			
-			
-		}
-	}	
+    public static void main(String[] args) {
+        
+        IParque parque = new Parque(); // Instanciar el parque
+        
+        char letra_puerta = 'A';
+        
+        System.out.println("¡Parque abierto!");
+        
+        // Crear y lanzar hilos para simular las entradas
+        for (int i = 0; i < 5; i++) {
+            String puerta = "" + ((char) (letra_puerta++));
+            ActividadEntradaPuerta entradas = new ActividadEntradaPuerta(puerta, parque);
+            new Thread(entradas).start();
+        }
+        
+        // Crear y lanzar hilos para simular las salidas
+        for (int i = 0; i < 5; i++) {
+            String puerta = "" + ((char) ('A' + i));
+            ActividadSalidaPuerta salidas = new ActividadSalidaPuerta(puerta, parque);
+            new Thread(salidas).start();
+        }
+    }   
 }
